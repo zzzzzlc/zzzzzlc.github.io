6dd07693041376d104e66fc76a1ba233a16eb9b4
zenglingchao <1925292044@qq.com>
2026-01-14 13:23:38 +0800

    增加tsconfig配置文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     server: {
@@ -7,7 +7,6 @@ export default defineConfig({
     },
     build: {
         outDir: "dist",
-        entry: "src/main.ts",
     },
     plugins: [
         // Add your plugins here
